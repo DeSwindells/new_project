9f494e9c6af24943abca2bd1d48df63509caec17
DeSwindells <157629458+DeSwindells@users.noreply.github.com>
2024-01-24 13:21:17 -0500

    Add files via upload

---
+++ first.py
@@ -0,0 +1,7 @@
+#First Python Program
+
+name = 'Demian'
+age = '27'
+goal = 'to advance my python skills to further my knowledge and implement these skills in a work based environment.'
+
+print("My name is " + name + ", I am " + age + " years old. And my goal for this course is " + goal)
