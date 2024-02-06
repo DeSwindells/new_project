f41dd5640bcf10af8e707e640e338cf5dc4ec662
DeSwindells <157629458+DeSwindells@users.noreply.github.com>
2024-02-05 19:37:08 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+# main.py RoboGarden Age Calculation
+# Demian Swindells 2/5/2024
+
+start_year = int(input("Enter the Year that either a Person or an Item was created:"))
+end_year = int(input("Enter the current year:"))
+age = end_year - start_year
+
+print(age)
