4d00f0e805a2de544eeff2454c309d26b6f982be
DeSwindells <157629458+DeSwindells@users.noreply.github.com>
2024-02-12 08:03:18 -0500

    Add files via upload

---
+++ Circle Area/main.py
@@ -0,0 +1,14 @@
+# main.py RoboGarden Circle Area and Circumfrence
+# Demian Swindells 2/6/2024
+
+radius = float(input("Enter the Radius of the Circle: "))
+
+# Circumfrence = 2 * Pie * Radius
+circumfrence = 2 * 3.14 * radius
+
+# Area = Pie * Radius Squared
+area = 3.14 * radius ** 2
+
+print("Radius: ", radius)
+print("Circumfrence: ", circumfrence)
+print("Area: ", area)
